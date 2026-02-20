a846b4e5a8eed897094968111f1a93aa84fa7ddf
smd-1 <skdanish1999@gmail.com>
2026-02-20 12:39:52 +0530

    Add all required functionality from feature lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,227 +1,96 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-const notes = [
-  {
-    id: 1,
-    title: "My Notes",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 2,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 3,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 4,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 5,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 6,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#0047FF",
-  },
-  {
-    id: 2,
-    title: "My CSS Notes",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 2,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 3,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 4,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 5,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-      {
-        id: 6,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#6691FF",
-  },
-  {
-    id: 3,
-    title: "Journal",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#FF79F2",
-  },
-  {
-    id: 4,
-    title: "JavaScript Group",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#B38BFA",
-  },
-  {
-    id: 5,
-    title: "My Notes",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#0047FF",
-  },
-  {
-    id: 6,
-    title: "My CSS Notes",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#6691FF",
-  },
-  {
-    id: 7,
-    title: "Journal",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#FF79F2",
-  },
-  {
-    id: 8,
-    title: "JavaScript Group",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#B38BFA",
-  },
-  {
-    id: 9,
-    title: "My Notes",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#0047FF",
-  },
-  {
-    id: 10,
-    title: "My CSS Notes",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#6691FF",
-  },
-  {
-    id: 11,
-    title: "Journal",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#FF79F2",
-  },
-  {
-    id: 12,
-    title: "JavaScript Group",
-    content: [
-      {
-        id: 1,
-        text: "Another productive way to use this tool to begin a daily writing routine. One way is to generate a random paragraph with the intention to try to rewrite it while still keeping the original meaning. The purpose here is to just get the writing started so that when the writer goes onto their day's writing projects, words are already flowing from their fingers.",
-        date: "9 Mar 2023",
-        time: "10:30 AM",
-      },
-    ],
-    backgroundColor: "#B38BFA",
-  },
-];
-
 function App() {
-  const [selectedNote, setSelectedNote] = useState(notes[0]);
+  const [notes, setNotes] = useState(() => {
+    const saved = localStorage.getItem("pocketNotes");
+    return saved ? JSON.parse(saved) : [];
+  });
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(null);
+  const [groupName, setGroupName] = useState("");
+  const [userInput, setUserInput] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("pocketNotes", JSON.stringify(notes));
+  }, [notes]);
+
+  const colorOptions = [
+    "#9B7AE5",
+    "#E765D8",
+    "#39C0D4",
+    "#E88963",
+    "#144EE3",
+    "#5B84E6",
+  ];
+  // Create Group function
+  const createGroup = (groupName, color) => {
+    if (!color) {
+      alert("Please choose a color before creating a group.");
+      return;
+    }
+    if (!groupName.trim()) {
+      alert("Please enter a group name before creating a group.");
+      return;
+    }
+    if (
+      notes.some((note) => note.title.toLowerCase() === groupName.toLowerCase())
+    ) {
+      alert("A group with this name already exists.");
+      return;
+    }
+    const newGroup = {
+      id: notes.length + 1,
+      title: groupName,
+      content: [],
+      backgroundColor: color,
+    };
+    setNotes([...notes, newGroup]);
+    setSelectedNote(newGroup);
+    setIsOpen(false);
+    setGroupName("");
+    setSelectedColor(null);
+  };
+
+  // Send button function
+  const handleSend = (id) => {
+    if (!userInput.trim()) {
+      alert("Please enter some text before sending.");
+      return;
+    }
+    const currentDate = new Date();
+    const newContent = {
+      id: selectedNote.content.length + 1,
+      text: userInput,
+      date: currentDate.toLocaleDateString("en-IN", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+      }),
+      time: currentDate.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+      }),
+    };
+    const updatedNotes = notes.map((note) => {
+      if (note.id === id) {
+        return {
+          ...note,
+          content: [...note.content, newContent],
+        };
+      }
+      return note;
+    });
+    setNotes(updatedNotes);
+    setSelectedNote((prevNote) => ({
+      ...prevNote,
+      content: [...prevNote.content, newContent],
+    }));
+    setUserInput("");
+  };
+
   return (
     <div className="App">
       {/* Left sidebar */}
@@ -233,7 +102,11 @@ function App() {
         {/* Notes list */}
         <div className="notes-list">
           {notes.map((note) => (
-            <div key={note.id} className="note-item">
+            <div
+              key={note.id}
+              className={`note-item ${selectedNote.id === note.id ? "active" : ""}`}
+              onClick={() => setSelectedNote(note)}
+            >
               <div
                 className="notes-initials"
                 style={{ backgroundColor: note.backgroundColor }}
@@ -250,6 +123,64 @@ function App() {
             </div>
           ))}
         </div>
+        <button className="add-btn" onClick={() => setIsOpen(true)}>
+          <svg
+            width="35"
+            height="37"
+            viewBox="0 0 35 37"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M34.1113 13.9453V21.3623H0V13.9453H34.1113ZM21.0547 0V36.2305H13.0908V0H21.0547Z"
+              fill="white"
+            />
+          </svg>
+        </button>
+        {isOpen && (
+          <div className="modal-overlay" onClick={() => setIsOpen(false)}>
+            <div className="modal" onClick={(e) => e.stopPropagation()}>
+              <h2>Create New group</h2>
+
+              <div className="form-row">
+                <label htmlFor="group-name">Group Name</label>
+                <input
+                  id="group-name"
+                  type="text"
+                  placeholder="Enter group name"
+                  value={groupName}
+                  onChange={(e) => setGroupName(e.target.value)}
+                  aria-label="Enter group name"
+                />
+              </div>
+
+              <div className="form-row">
+                <label htmlFor="color-choice">Choose colour</label>
+                <div className="colors">
+                  {colorOptions.map((color) => (
+                    <button
+                      key={color}
+                      className={`color-circle ${
+                        selectedColor === color ? "selected" : ""
+                      }`}
+                      style={{ backgroundColor: color }}
+                      onClick={() => setSelectedColor(color)}
+                      aria-label={`Select color ${color}`}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className="actions">
+                <button
+                  className="create-btn"
+                  onClick={() => createGroup(groupName, selectedColor)}
+                >
+                  Create
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </section>
       {/* main content */}
       <section className="main-content">
@@ -257,9 +188,9 @@ function App() {
         <div className="header">
           <div
             className="notes-initials"
-            style={{ backgroundColor: selectedNote.backgroundColor }}
+            style={{ backgroundColor: selectedNote?.backgroundColor }}
           >
-            {selectedNote.title
+            {selectedNote?.title
               .trim()
               .split(/\s+/)
               .slice(0, 2)
@@ -267,23 +198,20 @@ function App() {
               .join("")
               .toUpperCase()}
           </div>
-          <h2>{selectedNote.title}</h2>
+          <h2>{selectedNote?.title}</h2>
         </div>
         <div className="group-notes">
-          {selectedNote.content.length > 0 ? (
+          {selectedNote?.content.length > 0 &&
             selectedNote.content.map((content) => (
               <div key={content.id} className="note">
                 <p className="note-content">{content.text}</p>
                 <div className="timeline">
                   <p className="date">{content.date}</p>
                   <span className="dot"></span>
-                  <p className="time">{content.time}</p>
+                  <p className="time">{content.time.toUpperCase()}</p>
                 </div>
               </div>
-            ))
-          ) : (
-            <p>No content available for this note.</p>
-          )}
+            ))}
         </div>
         {/* input text area */}
         <div className="input-area">
@@ -291,8 +219,20 @@ function App() {
             name="user-input"
             id="user-input"
             placeholder="Enter your text here...."
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey && userInput.trim()) {
+                e.preventDefault();
+                handleSend(selectedNote.id);
+              }
+            }}
           ></textarea>
-          <div className="send-btn">
+          <button
+            className="send-btn"
+            onClick={() => handleSend(selectedNote.id)}
+            disabled={!userInput.trim()}
+          >
             <svg
               width="35"
               height="29"
@@ -302,10 +242,10 @@ function App() {
             >
               <path
                 d="M0 29V18.125L14.5 14.5L0 10.875V0L34.4375 14.5L0 29Z"
-                fill="#001F8B"
+                className="send-icon"
               />
             </svg>
-          </div>
+          </button>
         </div>
       </section>
     </div>
